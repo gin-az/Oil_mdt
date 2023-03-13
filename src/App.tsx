0a0cf365f6fc7a454d8d67c99dde7b6eb3a3c7e0
AGindullin <GA>
2023-03-13 13:42:19 +0300

    правка

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import React, { useRef } from 'react';
 import './App.css';
 import { IDataToDay, OilTable } from './components/OilTable';
-import { useActions } from './hooks/hooks/useAction';
+import { useActions } from './hooks/useAction';
 import { useSelector } from 'react-redux';
 import { Button } from 'primereact/button';
-import "primereact/resources/themes/lara-light-indigo/theme.css";
-import "primereact/resources/primereact.min.css";
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
 
 function App() {
   const { app__initApp } = useActions();
 
-  const oilData = useSelector((state: {data: {data: Array<IDataToDay> | null}}) => state.data);
+  const oilData = useSelector((state: { data: { data: Array<IDataToDay> | null } }) => state.data);
 
   return (
     <div className="wrap">
-      <Button label="Загрузить данные" severity="success" onClick={app__initApp}/>
-      { oilData.data ? <OilTable data={oilData.data} /> : <span>Данные отсутствуют, попробуйте загрузить.</span>}
+      <Button label="Загрузить данные" severity="success" onClick={app__initApp} />
+      {oilData.data ? <OilTable data={oilData.data} /> : <span>Данные отсутствуют, попробуйте загрузить.</span>}
     </div>
   );
 }
--- src/hooks/useAction.ts
+++ src/hooks/useAction.ts
@@ -1,7 +1,7 @@
 import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import * as DataActionCreators from '../../store/actions';
-import * as DataThunksCreators from '../../store/thunk';
+import * as DataActionCreators from '../store/actions';
+import * as DataThunksCreators from '../store/thunk';
 
 export const ActionCreators = {
   ...DataActionCreators,
